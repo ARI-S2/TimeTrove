687d095d5fdafadaf15db49c9af744420a2a5cdf
ARI-S2 <duswl966@gmail.com>
2024-11-08 04:03:31 +0900

    chore: API URL 수정

--- frontend/src/http-commons.js
+++ frontend/src/http-commons.js
@@ -4,7 +4,7 @@ import {KAKAO_AUTH_URL} from "./components/member/OAuth";
 
 // axios 인스턴스 생성
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_EC2_HOST_URL,
+    baseURL: `http://${process.env.REACT_APP_EC2_HOST_URL}`,
     headers: {
         "Content-Type": "application/json;charset=utf-8",
         "Access-Control-Allow-Origin": "*"
@@ -43,7 +43,7 @@ instance.interceptors.response.use(
                 const tokens = getCookie('jwtToken');
 
                 try {
-                    const response = await axios.post(process.env.REACT_APP_EC2_HOST_URL,+'/reissue', null, {
+                    const response = await axios.post(`http://${process.env.REACT_APP_EC2_HOST_URL}/reissue`, null, {
                         headers: {
                             'Authorization': `${tokens.refreshToken}`,
                         },
