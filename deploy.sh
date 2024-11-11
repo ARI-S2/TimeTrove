31c6e634c56553873aec1e0b3af199e14ca45990
ARI-S2 <duswl966@gmail.com>
2024-11-11 22:32:17 +0900

    feat: docker 자동 설치를 위해 depoly.sh 추가

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+# 도커가 없으면 설치
+if ! type docker > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install -y docker-ce
+  sudo usermod -aG docker $USER
+fi
+
+# 도커 컴포즈가 없으면 설치
+if ! type docker-compose > /dev/null
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+# Docker 서비스 시작
+sudo systemctl start docker
+sudo systemctl enable docker
