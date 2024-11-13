ae7ed71ea57734b38d9a78dea1f473df84d2008b
ARI-S2 <duswl966@gmail.com>
2024-11-14 08:04:06 +0900

    feat: 깃허브 시크릿을 활용한 리액트 환경변수 추가

--- frontend/src/components/member/OAuth.js
+++ frontend/src/components/member/OAuth.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "d008d7a526a3e250c2bd63146f16c578";
-const REDIRECT_URI = `http://${process.env.REACT_APP_EC2_HOST_URL || 'localhost:3000'}/api/login/oauth2/callback/kakao`;
+const CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+const REDIRECT_URI = `http://${process.env.REACT_APP_EC2_HOST_URI || 'localhost:3000'}/api/login/oauth2/callback/kakao`;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
