15e82437116207a5eeac5ca4bfb6464a35f10884
ARI-S2 <duswl966@gmail.com>
2024-11-14 19:13:56 +0900

    fix: 카카오 로그인 리다이렉트 uri 수정

--- frontend/src/components/member/OAuth.js
+++ frontend/src/components/member/OAuth.js
@@ -1,4 +1,4 @@
 const CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
-const REDIRECT_URI = `http://${process.env.REACT_APP_EC2_HOST_URI || 'localhost:3000'}/api/login/oauth2/callback/kakao`;
+const REDIRECT_URI = `http://${process.env.REACT_APP_EC2_HOST_URI || 'localhost:3000'}/login/oauth2/callback/kakao`;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
